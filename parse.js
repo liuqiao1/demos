d875db8cb9212363d80f45de5419214e5eb24ac4
liuqiao <liuqiao_i@didichuxing.com>
2018-04-20 08:19:28 +0800

    dev 1  commit

--- parse.js
+++ parse.js
@@ -1,3 +1,4 @@
 // const JsonStr="{'cells':[{'cell_type': 'code', 'execution_count': 1, 'metadata': {'collapsed': True}, 'outputs': [{'output_type': ''}], 'source': ['n=10'], 'index': 0}]}";
 const JsonStr='{"cells":[1]}';
 console.log(JSON.parse(JsonStr));
+// ok
